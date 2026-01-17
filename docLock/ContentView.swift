ae9c66e89577047df4693f748d812855682e4eea
Pranav katiyar <pranav.katiyar@decimal.co.in>
2026-01-17 11:50:44 +0530

    Initial Commit

---
+++ docLock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  docLock
+//
+//  Created by Pranav Katiyar on 17/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ docLock/docLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  docLockApp.swift
+//  docLock
+//
+//  Created by Pranav Katiyar on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct docLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
